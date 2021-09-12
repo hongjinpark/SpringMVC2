088497573700acaae95c19efb2e04d992fb20e94
ektkfk180 <81549627+ektkfk180@users.noreply.github.com>
2021-09-12 21:22:08 +0900

    Feat BasicController
    javascript 메서드 추가

--- src/main/java/hello/thymeleaf/basic/BasicController.java
+++ src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -109,6 +109,12 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("UserA",10));
+        addUsers(model);
+        return "basic/javascript";
+    }
     @GetMapping("/comments")
     public String comments(Model model) {
         model.addAttribute("data","Spring!");
